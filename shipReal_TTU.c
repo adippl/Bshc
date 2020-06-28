b1f8fc20c83bf5b7b46f1d2ea47a63709c495a77
ev <adip@cumallover.me>
2020-06-28 04:32:21 +0200

    shipReal arr template added

---
+++ shipReal_TTU.c
@@ -0,0 +1,7 @@
+#include "shipReal.h"
+
+#ifdef T
+#undef T
+#endif
+#define T obj_shipReal
+#include "submod/cstl/tmpl/arr.c"
---
+++ shipReal_TTU.h
@@ -0,0 +1,13 @@
+#ifndef _SHIP_TLU_H_
+#define _SHIP_TLU_H_
+
+#include "conf.h"
+#include "shipReal.h"
+
+#ifdef T
+#undef T
+#endif
+#define T obj_shipReal
+#include "submod/cstl/tmpl/arr.h"
+
+#endif //_SHIP_TLU_H_
